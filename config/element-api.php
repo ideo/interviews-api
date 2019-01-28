4aebd3aa06d17c532a2d2f3999d5afdd83cfa00b
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2019-01-28 13:18:57 +0200

    error fixed

--- config/element-api.php
+++ config/element-api.php
@@ -6,80 +6,81 @@ use craft\elements\Asset;
 use craft\helpers\UrlHelper;
 
 return [
-  'endpoints' => [
-    'interviews.json' => /**
-     * @return array
-     */
-    function () {
-      Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
+'endpoints' => [
+'interviews.json' => /**
+ * @return array
+ */
+function () {
+  Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
 
+  return [
+  'elementType' => Entry::class,
+  'criteria'    => ['section' => 'interviews'],
+  'transformer' => function (Entry $entry) {
+    return [
+    'title'     => $entry->title,
+    'person'    => array_map(function (Category $category) {
       return [
-        'elementType' => Entry::class,
-        'criteria'    => ['section' => 'interviews'],
-        'transformer' => function (Entry $entry) {
-          return [
-            'title'  => $entry->title,
-            'person' => array_map(function (Category $category) {
-              return [
-              'title' => $category->title,
-              'age' => $category->age,
-              'city' => $category->city,
-              'persondescription' => $category->persondescription,
-              ];
-            }, $entry->person->find()),
-            'video' => array_map(function (Asset $asset) {
-              return [
-              'filename' => $asset->filename,
-              ];
-            }, $entry->video->find()),
-            'thumbnail' => array_map(function (Asset $asset) {
-              return [
-              'filename' => $asset->filename,
-              ];
-            }, $entry->thumbnail->find()),
-            'theme' => array_map(function (Category $category) {
-              return [
-              'title' => $category->title,
-              'themedescription' => $category->themedescription,
-              ];
-            }, $entry->theme->find())
-          ];
-        },
+      'title'             => $category->title,
+      'age'               => $category->age,
+      'city'              => $category->city,
+      'persondescription' => $category->persondescription,
       ];
-    },
-    'people.json' =>
-    function() {
-      Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
+    }, $entry->person->find()),
+    'video'     => array_map(function (Asset $asset) {
+      return [
+      'filename' => $asset->filename,
+      ];
+    }, $entry->video->find()),
+    'thumbnail' => array_map(function (Asset $asset) {
+      return [
+      'filename' => $asset->filename,
+      ];
+    }, $entry->thumbnail->find()),
+    'theme'     => array_map(function (Category $category) {
+      return [
+      'title'            => $category->title,
+      'themedescription' => $category->themedescription,
+      ];
+    }, $entry->theme->find())
+    ];
+  },
+  ];
+},
+'people.json'     =>
+function () {
+  Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
 
-      return [
-        'elementType' => Category::class,
-        'criteria' => ['group' => 'person'],
-        'transformer' => function(Category $category) {
-          return [
-            'title' => $category->title,
-            'age' => $category->age,
-            'city' => $category->city,
-            'description' => $category->persondescription,
-            'videos' => []
-          ];
-        }
-      ];
-    },
-    'themes.json' =>
-    function() {
-      Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
-      return [
-        'elementType' => Category::class,
-        'criteria' => ['group' => 'theme'],
-        'transformer' => function(Category $category) {
-          return [
-            'title' => $category->title,
-            'description' => $category->themescription,
-            'videos' => []
-          ];
-        }
-      ]
-    ]
-];
+  return [
+  'elementType' => Category::class,
+  'criteria'    => ['group' => 'person'],
+  'transformer' => function (Category $category) {
+    return [
+    'title'       => $category->title,
+    'age'         => $category->age,
+    'city'        => $category->city,
+    'description' => $category->persondescription,
+    'videos'      => []
+    ];
+  }
+  ];
+},
+'themes.json'     =>
+function () {
+  Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
+  return [
+  'elementType' => Category::class,
+  'criteria'    => ['group' => 'theme'],
+  'transformer' => function (Category $category) {
+    return [
+    'title'       => $category->title,
+    'description' => $category->themescription,
+    'videos'      => []
+    ];
+  }
+  ];
+}
+]
+]
 
 ?>
